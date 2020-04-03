f81423289954573ff071f1e094dc27c3677a8772
royweb <xytank@gmail.com>
2020-04-03 15:31:20 -0400

    Clear the global.css

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
 	let name = "Roy";
+	let points = 100;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-</main>
+<h1>Hello {name}!</h1>
+<h3>{points}</h3>
 
 <style>
-
+	h1{
+		color: #1b11a5;
+	}
 </style>
