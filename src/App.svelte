1ff689f4a361ecd7209797e3a9721b82e165dedc
royweb <xytank@gmail.com>
2020-04-04 09:49:55 -0400

    Display player information

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,18 @@
 <script>
+	// variables
 	let name = "Roy";
 	let points = 100;
 </script>
-
-<h1>Hello {name}!</h1>
-<h3>{points}</h3>
 
 <style>
 	h1{
 		color: #1b11a5;
 	}
 </style>
+
+<div class="container">
+	<div class="card">
+		<h1>Hello {name}!</h1>
+		<h3>{points}</h3>
+	</div>
+</div>
