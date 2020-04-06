fb1801fe35143b0b1698afa6bc56019c38641b53
royweb <xytank@gmail.com>
2020-04-06 15:08:06 -0400

    Use #each to loop through array

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,31 @@
 <script>
 	import Navbar from './Navbar.svelte';
 	import Player from './Player.svelte';
+
+	let players = [
+		{
+			name: "Roy Web",
+			points: 100
+		},
+		{
+			name: "Mike Doe",
+			points: 88
+		},
+		{
+			name: "Jill Smith",
+			points: 91
+		}
+	];
+
 </script>
 
 <Navbar />
 <div class="container">
-	<Player />
+	{#if players.length === 0}
+		<p>No Players yet</p>
+	{:else}
+		{#each players as player}
+			<Player />
+		{/each}
+	{/if}
 </div>
