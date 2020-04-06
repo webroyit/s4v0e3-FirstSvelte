9e5197a073004bac4f34e5c9576cb074858e6ccf
royweb <xytank@gmail.com>
2020-04-06 11:29:30 -0400

    Add buttons

--- src/App.svelte
+++ src/App.svelte
@@ -14,5 +14,7 @@
 	<div class="card">
 		<h1>Hello {name}!</h1>
 		<h3>{points}</h3>
+		<button class="btn" on:click={addPoint}>+1</button>
+		<button class="btn btn-dark" on:click={removePoint}>-1</button>
 	</div>
 </div>
