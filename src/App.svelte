7e8d0fd3bff0272942a8addfa5a7abca91f8bcaa
royweb <xytank@gmail.com>
2020-04-06 12:48:47 -0400

    Use bind:value to bind the input to points variable

--- src/App.svelte
+++ src/App.svelte
@@ -11,6 +11,10 @@
 	h1{
 		color: #1b11a5;
 	}
+
+	h3{
+		margin-bottom: 1rem;
+	}
 </style>
 
 <div class="container">
@@ -19,5 +23,7 @@
 		<h3>{points}</h3>
 		<button class="btn" on:click={addPoint}>+1</button>
 		<button class="btn btn-dark" on:click={removePoint}>-1</button>
+		<!-- bind:value for two way binding -->
+		<input type="number" bind:value={points} />
 	</div>
 </div>
